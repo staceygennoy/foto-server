a52e45318ae87bf4c1b454fd266262f862781fc3
James Gennoy <james@gennoy.com>
2018-06-23 15:05:15 -0700

    Updating libraries (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as fs from 'fs';
-import { Observable } from 'rxjs';
+import { timer } from 'rxjs';
 import { argv } from 'yargs';
 
 const PORT = 3001;
@@ -10,7 +10,17 @@ const UPDATE_FILES = 60 * 60 * 1000; // 1 hour
 const app = express();
 let files: string[] = [];
 
-Observable.timer(0, UPDATE_FILES).subscribe(() => files = fs.readdirSync(DIR));
+timer(0, UPDATE_FILES).subscribe(() => {
+  const startCount = files.length;
+  files = fs.readdirSync(DIR).filter(file => !file.startsWith('.'));
+  const endCount = files.length;
+  if (startCount < endCount) {
+    console.log(endCount - startCount + ' photos added.');
+  }
+  if (startCount > endCount) {
+    console.log(startCount - endCount + ' photos removed.');
+  }
+});
 
 app.get('/api/image/random', (req, res) => {
   const randomIndex = Math.floor((Math.random() * files.length));
